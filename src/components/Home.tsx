c429212af42c472488b5b056ff0cf2dd08d17e8d
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-05-11 20:49:25 +0530

    update resume

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -34,7 +34,7 @@ export function Home() {
             <Link
               target="_blank"
               href={
-                "https://drive.google.com/file/d/1ECz4dnIgIzcnIuERbRa1Etpm-hSP_A6t/view?usp=sharing"
+                "https://drive.google.com/file/d/17NKNclA2l9Xc_AmJYOsCPWad7f3BTx8f/view?usp=sharing"
               }
             >
               <HoverBorderGradient
